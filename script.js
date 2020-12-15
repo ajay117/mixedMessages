ec5753284b0bcf6cb33dc81341818c46b5ec5a6e
Ajay kc <meajay64@gmail.com>
2020-12-15 10:33:40 +0900

    Now the program shows some stage 1 random messages

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+/*
+You will + find your true love + .
+You will + advance in your career + .
+You will + going to excel wherever you go + .
+You will + marry multiple times + .
+You will + become a billionaire + .
+*/
 
- function add(num) {
-     return num + num;
- }
- console.log(add(2));
+let messages = ['find your true love', 'advance in your career',
+'going to excel wherever you go', 'marry multiple times', 'become a billionaire', 'earn a lot of money'];
+let randomNumber = Math.floor(Math.random() * 5);
+let randomMessages = 'You will ' + messages[randomNumber] + '.';
+console.log(randomMessages);
