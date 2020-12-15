9eab9152753a4ebd61c0e80a7d053d66b60750c8
Ajay kc <meajay64@gmail.com>
2020-12-15 10:37:23 +0900

    Deleting some comments from the program

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-/*
-You will + find your true love + .
-You will + advance in your career + .
-You will + going to excel wherever you go + .
-You will + marry multiple times + .
-You will + become a billionaire + .
-*/
+
 
 let messages = ['find your true love', 'advance in your career',
 'going to excel wherever you go', 'marry multiple times', 'become a billionaire', 'earn a lot of money'];
